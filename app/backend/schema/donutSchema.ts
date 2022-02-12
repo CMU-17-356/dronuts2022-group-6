80cc6206e6f11e23d0a77a7273408b0b4eafac3c
tlee0818 <texeirareborn@gmail.com>
2022-02-12 18:19:51 -0500

    added tests for donut schema

--- app/backend/schema/donutSchema.ts
+++ app/backend/schema/donutSchema.ts
@@ -13,14 +13,33 @@ const donutSchema = new Schema({
     }, //Salted, Hashed Password
     price: {
         type: Number,
+        validate:{
+            validator: (v) => {
+                return v >= 0
+            },
+            message: `price cannot less than 0!`
+        },
         required: [true, "price required"]
     },
     quantity_left: {
         type: Number,
+        default: 0,
+        validate:{
+            validator: (v) => {
+                return v >= 0
+            },
+            message: `quantity left cannot less than 0!`
+        },
         required: [true, "quantity left required"]
     },
     weight: {
         type: Number,
+        validate:{
+            validator: (v) => {
+                return v >= 0
+            },
+            message: `weight cannot less than 0!`
+        },
         required: [true, "weight required"]
     },
 });
