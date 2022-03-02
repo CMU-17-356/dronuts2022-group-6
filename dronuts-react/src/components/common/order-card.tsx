9a48926ff562209f3836aca51242a00e082aaee9
Varshaa <vsekarja@andrew.cmu.edu>
2022-03-02 08:56:57 -0500

    order page

--- dronuts-react/src/components/common/order-card.tsx
+++ dronuts-react/src/components/common/order-card.tsx
@@ -1,34 +1,33 @@
-import {Card, Text, Divider, Badge, Checkbox} from '@geist-ui/react';
+import {Card, Text, Divider, Badge, Checkbox, Grid} from '@geist-ui/react';
 import React from 'react';
 
-function OrderCardComponent() {
-  return (
-    <Card shadow width="100%" hoverable className='donut-info'>
-      <div>
-        <h3 className='donut-name'>Order #453245</h3>
-        <h4 className='donut-name'>Drone: D3</h4>
-        <h4 className='donut-name'>Time of Order: 7:00AM</h4>
-      </div>
-      <Divider />
-      <Text>
-        Glazed w/ Sprinkles <b>x 2</b>
-      </Text>
-      <Text>
-        Glazed <b>x 2</b>
-      </Text>
-      <Text>
-        Cherry filling <b>x 2</b>
-      </Text>
-      <Text>
-        Banana Cream Filling <b>x 2</b>
-      </Text>
-      <Divider />
-      <Badge style={ {backgroundColor: '#ef72ac'} }>$32.00</Badge>
-      <div className='amount-input'>
-        <Checkbox checked={false}>Completed</Checkbox>
-      </div>
-    </Card>
-  );
+function OrderCardComponent(data: any) {
+  if (data.data) {
+    return (
+      <Card shadow width="100%" hoverable className='order-info'>
+      
+        <Text h4 className='order-name'>{data.data.name}
+        </Text>
+        <Divider />
+        
+        <Grid><h5>{data.data.amount} x</h5></Grid>
+          <Grid>
+            <Badge style={ {backgroundColor: '#ef72ac'} }>
+            ${data.data.price}
+            </Badge></Grid>
+        <Divider />
+        {/* <Badge style={ {backgroundColor: '#ef72ac'} }>$32.00</Badge> */}
+        <div className='amount-input'>
+          <Checkbox checked={false}>Completed</Checkbox>
+        </div>
+      </Card>
+    );
+
+  }
+  else {
+    return <p>Donut Not Available</p>;
+}
 }
 
 export default OrderCardComponent;
+
