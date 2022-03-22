566b5202d7c4e519eecf2bf66b74a8853261ccc3
Varshaa <vsekarja@andrew.cmu.edu>
2022-03-22 17:46:21 -0400

    added react integration component, without installation

---
+++ dronuts-react/src/components/common/simple-map.tsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Map, {GoogleApiWrapper} from 'google-maps-react';
+
+
+class SimpleMap extends Component {
+  render() {
+      return(
+          <Map
+          google={this.props.google}
+          style = {{width: "100%", height: "100%"}}
+          zoom = {10}
+          initialCenter = {{
+            lat: 59.95,
+            lng: 30.33
+          }
+          }
+          />
+
+       
+      );
+  }  
+}
+
+export default GoogleApiWrapper({
+    apiKey: "AIzaSyBZGs8VeKxPywlezjgTEuULvRRz2-F3zC0"
+})(SimpleMap);
+
